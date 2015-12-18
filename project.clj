438f9ddbfd12c546acf5b35e15651e9b701f6c02
Christopher Mark Gore <chris.gore@climate.com>
2015-12-18 11:50:48 -0600

    Adding dependency info for gs-wcs.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :repositories [["boundlessgeo.com" "https://repo.boundlessgeo.com/main"]
+                 ["osgeo.org" "http://download.osgeo.org/webdav/geotools/"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.geoserver/gs-wcs "2.8.1"]])
