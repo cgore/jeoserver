992da180400fe744d238bbcf5c0cc3ee55ef9328
Christopher Mark Gore <chris.gore@climate.com>
2015-12-18 13:34:22 -0600

    Beginning a simple wrapper around WCSInfoImpl.

---
+++ src/jeoserver/wrap/wcs/wcs_info_impl.clj
@@ -0,0 +1,29 @@
+(ns jeoserver.wrap.wcs.wcs-info-impl
+  (:import org.geoserver.wcs.WCSInfoImpl))
+
+(defn wcs-info-impl? [thing]
+  (instance? WCSInfoImpl thing))
+
+(defn gml-prefixing? [info]
+  (.isGMLPrefixing info))
+
+(defn set-gml-prefixing!
+  ([info]
+   (set-gml-prefixing! info true))
+  ([info value]
+   (.setGMLPrefixing info value)))
+
+(defn unset-gml-prefixing! [info]
+  (set-gml-prefixing! info false))
+
+(defn max-input-memory [info]
+  (.getMaxInputMemory info))
+
+(defn set-max-input-memory! [info value]
+  (.setMaxInputMemory info value))
+
+(defn max-output-memory [info]
+  (.getMaxOutputMemory info))
+
+(defn set-max-output-memory! [info value]
+  (.setMaxOutputMemory info value))
